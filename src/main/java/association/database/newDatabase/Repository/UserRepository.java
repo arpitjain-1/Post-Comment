f5c1573fe8d958f262db0d17f61fbf853c0cc582
Arpit Jain <arpitjainkhi56@gmail.com>
2025-06-17 14:50:08 +0530

    Added JPA

--- src/main/java/association/database/newDatabase/Repository/UserRepository.java
+++ src/main/java/association/database/newDatabase/Repository/UserRepository.java
@@ -1,6 +1,11 @@
 package association.database.newDatabase.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import association.database.newDatabase.Entity.UserModel;
@@ -11,5 +16,13 @@ public interface UserRepository extends JpaRepository <UserModel, Integer>{
     association.database.newDatabase.Entity.UserModel save(association.database.newDatabase.Entity.UserModel user);
 
     boolean existsByEmail(String email);
+
+    // Creating JPA
+    @Query("Select u from UserModel u")
+    public List<UserModel> getAllUser();
+
+    //Jpa with the Qery Perameter
+    @Query("Select u from UserModel u where u.id =:id")
+    Optional<UserModel> getUserByID(@Param(value = "id") int id); 
     
 }
